cd7ecc422f687f0b83de25010d3607e9634dbe62
mozaika228 <mozaika228@gmail.com>
2026-02-04 21:26:25 +0500

    Fix auth test expectation

--- backend/src/test/java/com/example/taskmanager/TaskControllerTest.java
+++ backend/src/test/java/com/example/taskmanager/TaskControllerTest.java
@@ -24,7 +24,7 @@ class TaskControllerTest {
     @Test
     void getTasks_requiresAuth() throws Exception {
         mockMvc.perform(get("/api/tasks"))
-            .andExpect(status().isUnauthorized());
+            .andExpect(status().isForbidden());
     }
 
     @Test
